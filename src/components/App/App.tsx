955d5c19e28638591ae473237b9c1fdb1be04f37
Nastasia151 <NastaZhuZhu@yandex.ru>
2025-07-04 14:09:59 +0300

    вынести app в отдельную папку

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,8 @@
 import { CSSProperties, useRef } from "react";
-import styles from '../styles/index.module.scss';
+import styles from 'src/styles/index.module.scss';
 import { defaultArticleState } from "src/constants/articleProps";
-import { ArticleParamsForm } from "./article-params-form/ArticleParamsForm";
-import { Article } from "./article/Article";
+import { ArticleParamsForm } from "../article-params-form/ArticleParamsForm";
+import { Article } from "../article/Article";
 
 export const App = () => {
 	const mainRef = useRef<HTMLDivElement>(null);
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import './styles/index.scss';
-import { App } from './components/App';
+import { App } from './components/App/App';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
